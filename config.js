9c9dff16af27f9b6087df2a8c11dd8915cad5c31
algabsil <76498959+algabsil@users.noreply.github.com>
2026-07-01 21:31:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // 4) Copiá la clave "anon public" y pegala abajo en SUPABASE_ANON_KEY.
 // 5) Corré el SQL que te paso en el mensaje (sección "SQL Editor" del proyecto) UNA sola vez.
 
-const SUPABASE_URL = "https://mikgyucqdoqvzqvbjxis.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://mikgyucqdoqvzqvbjxis.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pa2d5dWNxZG9xdnpxdmJqeGlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4MzMwNTQsImV4cCI6MjA5ODQwOTA1NH0.sqdU7KKDl27b1L71MTleRJCs4Zjk76t8YPo6yKvQG7k";
